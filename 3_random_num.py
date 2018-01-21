7286a5ade26f0316a3a3dea72d3b588c07b06887
Derek Ogletree <derek.ogletree@gmail.com>
2018-01-21 15:31:06 -0500

    generate 3 rand nums, break if == 5

---
+++ 3_random_num.py
@@ -0,0 +1,20 @@
+'''
+generate three random numbers.
+if one of the numbers is 5, break
+the loop.
+ '''
+import random
+
+print "Lucky Numbers! 3 numbers will be generated."
+print "If one of them is a '5', you lose!"
+
+count = 0
+while count < 3:
+  num = random.randint(1, 6)
+  print num
+  if num == 5:
+    print "Sorry, you lose!"
+    break
+  count += 1
+else:
+  print "You win!"
